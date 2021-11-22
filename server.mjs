e4b9246cea04bf2f644c651155e9d2460ee66117
Owais Afsar <owais.afsar@outlook.com>
2021-11-22 18:08:42 +0500

    ...

--- server.mjs
+++ server.mjs
@@ -2,34 +2,41 @@ import express from "express";
 import morgan from 'morgan';
 import cors from 'cors';
 import * as vision from '@google-cloud/vision';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+// app.use(express.json({ limit: '100mb' }));
 app.use(morgan('short'));
 
 const port = process.env.PORT || 3000;
 const data = 'Welcome to Heroku App.';
 
 app.use((req, res, next) => {
-    console.log(`Request comes, ${req.body}`);
+    console.log(`New request comes`);
+    console.log(req.body);
     next();
-})
+});
+// app.use(bodyParser.json({ limit: '100mb' }));
+// app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
 
 app.get(`/intro`, (req, res) => {
     res.send(data);
 });
 
-app.get(`/recog`, async (req, res) => {
+app.post(`/recog`, express.json({ limit: 104857600 }), async (req, res) => {
 
+    console.log(req.body);
     try {
         const client = new vision.ImageAnnotatorClient();
         // console.log(client);
         const fileName = './blob/main/resources/handwritten.jpg';
+        // const fileName = req.body.image;
         console.log(fileName);
         const [result] = await client.documentTextDetection(fileName);
         console.log(result);
